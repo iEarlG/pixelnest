741b50bed2f4a1def685e081a374ec010f738c7f
Earl A. Villapaz <jasonburning16@gmail.com>
2023-08-22 19:07:23 +0800

    session part 3

--- lib/session.ts
+++ lib/session.ts
@@ -6,7 +6,8 @@ import { JWT } from "next-auth/jwt";
 import GoogleProvider from "next-auth/providers/google";
 import jswonwebtoken from "jsonwebtoken";
 
-import { SessionInterface } from "@/types/common";
+import { SessionInterface, UserProfile } from "@/types/common";
+import { createUser, getUser } from "@/action/actions";
 
 export const authOptions: NextAuthOptions = {
     providers: [
@@ -33,9 +34,15 @@ export const authOptions: NextAuthOptions = {
         },
         async signIn({ user }: { user: AdapterUser | User }) {
             try {
-                // get the users if they exist
+                const userExists = await getUser(user?.email as string) as { user?: UserProfile };
 
-                // if they don't exist, we create them
+                if (!userExists.user) {
+                    await createUser(
+                        user.name as string,
+                        user.email as string,
+                        user.image as string
+                    );
+                }
 
                 // return true if they exist or were created successfully
                 return true;
